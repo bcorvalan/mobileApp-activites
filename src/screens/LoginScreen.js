b2340edf327019e954bf631df09a7aa80868f9fa
Belen Corvalan <bcorvalan@4implus.com>
2020-12-03 18:08:28 -0300

    navigationSetUp

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginScreen = props =>{
+    retur(
+        <View style={styles.screen}>
+        <Text>Login Screen</Text>
+      </View> 
+    );
+};
+const styles = StyleSheet.create({
+    screem: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+  });
+
+  export default LoginScreen;
